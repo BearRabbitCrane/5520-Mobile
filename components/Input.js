fa856c75b5247a51b8c7654fb9dd27fa5bf4a797
Yihe Xiong <xiong.yihe@northeastern.edu>
2024-09-13 14:25:56 +0800

    Update the Input component and the TextInput component to fulfill Lab1

--- components/Input.js
+++ components/Input.js
@@ -1,19 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function Input() {
+export default function Input({ autoFocus }) {
   const [text, setText] = useState("");
+  const [isBlurred, setIsBlurred] = useState(false); 
+
   return (
     <View>
-     <TextInput 
+      <TextInput 
         placeholder='Type sth' 
         keyboardType='default' 
-        style={{borderBottomColor: 'purple', borderBottomWidth: 2}}
+        style={styles.input}
         value={text}
+        autoFocus={autoFocus} 
         onChangeText={function (changedText) {
           setText(changedText);
+          setIsBlurred(false);
         }}
+        onBlur={() => setIsBlurred(true)} 
       />
+
+      {text.length > 0 && !isBlurred && (
+        <Text>Character count: {text.length}</Text>
+      )}
+
+      {isBlurred && (
+        <Text>
+          {text.length >= 3 ? "Thank you" : "Please type more than 3 characters"}
+        </Text>
+      )}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderBottomColor: 'purple',
+    borderBottomWidth: 2,
+    marginBottom: 10
+  }
+});
